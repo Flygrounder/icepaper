513f96ebb1c24346a09ab5e8e1707e220c63d4f3
Artyom Belousov <flygrounder@yandex.ru>
2025-02-20 21:59:14 +0300

    Add iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{Column, column, text};
+
+#[derive(Default)]
+struct State {}
+
+impl State {
+    fn update(&mut self, _: ()) {}
+
+    fn view(&self) -> Column<()> {
+        column![text("Hello, world!")]
+    }
+}
+
+fn main() -> iced::Result {
+    iced::run("Background Core", State::update, State::view)
 }
